da213d2ad29f13e8073ddec2116df83389254197
MarcoTz <38535746+MarcoTz@users.noreply.github.com>
2024-05-01 12:38:21 +0000

    Deploying to gh-pages from @ MarcoTz/Recipes@18a409dc93d117422ffae5a91e06984b459a05fe 🚀

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-function filterRecipes() {
-  recipes = document.getElementById('recipe_list').children;
-  if (typeof(recipes) == 'undefined' || recipes === null) { return; } 
-  search_value = document.getElementById('search').value.toLowerCase();
-  if (typeof(search_value) == 'undefined' || search_value === null) { return; } 
- 
-  for(recipe of recipes){
-    recipe_name = recipe.textContent.toLowerCase();
-    recipe_tags = ""
-    for(child of recipe.children) {
-      if(child.className == 'recipe_taglist') { recipe_tags = child.textContent; break;}
-    }
+function filter(){
+  if(document.body.id == 'body_index') { filter_recipes(); }
+  if(document.body.id == 'body_tag_overview') { filter_tags(); }
+}
 
+function filter_general(html_elements,filter_fun) {
+  search_value = document.getElementById('search').value.toLowerCase();
+  for(element of html_elements){
+    if(filter_fun(element,search_value)) { element.style.display='block';}
+    else { element.style.display='none'; }
+  }
+}
+
+function filter_recipes() {
+  recipes = document.getElementById('recipe_list').children;
+
+  filter_fun = function(recipe_element,search_value) {
+    recipe_name = recipe_element.textContent.toLowerCase();
+    recipe_tags = ''
+    for(child of recipe_element.children) {
+      if(child.className == 'recipe_taglist') { recipe_tags = child.textContent; break; }
+    }
     matches_name = recipe_name.includes(search_value);
     matches_tags = recipe_tags.includes(search_value);
-    
-    matches = matches_name || matches_tags 
-    if(matches) { recipe.style.display="block"; }
-    else { recipe.style.display="none"; }
+    return matches_name || matches_tags;
+  };
+
+  filter_general(recipes,filter_fun);
+ 
+}
+
+function filter_tags() {
+  tags = document.getElementById('tags_list').children;
+
+  filter_fun = function(tagElement,search_value) {
+    tag_text = tagElement.textContent.toLowerCase();
+    return tag_text.includes(search_value);
   }
+
+  filter_general(tags,filter_fun);
 }
