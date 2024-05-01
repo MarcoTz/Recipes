2dedd9ecc116feda7adf7d98e239bbf623dec972
MarcoTz <38535746+MarcoTz@users.noreply.github.com>
2024-05-01 11:43:21 +0000

    Deploying to gh-pages from @ MarcoTz/Recipes@10a8e917d6b82c2736b6ddd11088e447217fa015 🚀

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 function filterRecipes() {
   recipes = document.getElementById('recipe_list').children;
+  if (typeof(recipes) == 'undefined' || recipes === null) { return; } 
   search_value = document.getElementById('search').value.toLowerCase();
-  
+  if (typeof(search_value) == 'undefined' || search_value === null) { return; } 
+ 
   for(recipe of recipes){
     recipe_name = recipe.textContent.toLowerCase();
-    if(!recipe_name.includes(search_value)){
-      recipe.style.display = "none";
-    } else { recipe.style.display = "block"; }
+    recipe_tags = ""
+    for(child of recipe.children) {
+      if(child.className == 'recipe_taglist') { recipe_tags = child.textContent; break;}
+    }
+
+    matches_name = recipe_name.includes(search_value);
+    matches_tags = recipe_tags.includes(search_value);
+    
+    matches = matches_name || matches_tags 
+    if(matches) { recipe.style.display="block"; }
+    else { recipe.style.display="none"; }
   }
 }
