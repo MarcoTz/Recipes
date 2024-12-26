f8e1da4ce35074fb863ee09ebf6ac3eb36d007df
MarcoTz <marco.tzschentke@gmail.com>
2024-12-26 19:17:44 +0100

    added recipe page

--- lib/pages/src/recipe/recipe_images.rs
+++ lib/pages/src/recipe/recipe_images.rs
@@ -1,10 +1,36 @@
 use crate::PageComponent;
-use html::elements::HtmlElement;
+use html::{
+    attribute::Attribute,
+    elements::{Div, HtmlElement, Img},
+};
+use std::rc::Rc;
 
-pub struct RecipeImages {}
+pub struct RecipeImages {
+    pub image_urls: Vec<String>,
+}
 
 impl PageComponent for RecipeImages {
-    fn render(self, date_format: &str) -> HtmlElement {
-        todo!()
+    fn render(self, _: &str) -> HtmlElement {
+        let img_divs: Vec<HtmlElement> = self
+            .image_urls
+            .into_iter()
+            .map(|url| {
+                Div {
+                    attributes: vec![Attribute::Class(vec!["recipe_image".to_owned()])],
+                    content: Rc::new(
+                        Img {
+                            attributes: vec![Attribute::Src(url)],
+                        }
+                        .into(),
+                    ),
+                }
+                .into()
+            })
+            .collect();
+        Div {
+            attributes: vec![Attribute::Id("recipe_images".to_owned())],
+            content: Rc::new(img_divs.into()),
+        }
+        .into()
     }
 }
